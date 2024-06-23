a1465dcad6b8cb9e70c8a02d57181657b8702e6d
AlexGVP <i202122156@cibertec.edu.pe>
2024-06-23 11:11:45 -0500

    first commit

---
+++ src/main/java/pe/edu/cibertec/T2_SW_AlexandraVilchez/T2SwAlexandraVilchezApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.T2_SW_AlexandraVilchez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T2SwAlexandraVilchezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T2SwAlexandraVilchezApplication.class, args);
+	}
+
+}
