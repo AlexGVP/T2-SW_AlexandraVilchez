04bfa37ef2f25107678a28f75e49b3334f7f3cbc
AlexGVP <i202122156@cibertec.edu.pe>
2024-06-23 12:37:10 -0500

    creando segundo api - pregunta 3

--- src/main/java/pe/edu/cibertec/T2_SW_AlexandraVilchez/controller/PersonajeController.java
+++ src/main/java/pe/edu/cibertec/T2_SW_AlexandraVilchez/controller/PersonajeController.java
@@ -1,4 +1,62 @@
 package pe.edu.cibertec.T2_SW_AlexandraVilchez.controller;
 
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pe.edu.cibertec.T2_SW_AlexandraVilchez.exception.ResourceNotFoundException;
+import pe.edu.cibertec.T2_SW_AlexandraVilchez.model.bd.Personaje;
+import pe.edu.cibertec.T2_SW_AlexandraVilchez.service.PersonajeService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping(path = "api/v1/personaje")
 public class PersonajeController {
+    private PersonajeService personajeService;
+
+    @GetMapping("")
+    public ResponseEntity<List<Personaje>> listarPersonaje(){
+        List<Personaje> personajeList = new ArrayList<>(personajeService.listarPersonaje());
+        if(personajeList.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(personajeList, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Personaje> obtenerPersonajeXId(
+            @PathVariable Integer id){
+        Personaje personaje = personajeService
+                .obtenerPersonajexId(id).orElseThrow(
+                        () -> new ResourceNotFoundException("El personaje con Id " +
+                                id + " no existe"));
+        return new ResponseEntity<>(personaje, HttpStatus.OK);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Personaje> registrarPersonaje(
+            @RequestBody Personaje personaje
+    ){
+        return new ResponseEntity<>(
+                personajeService.guardarPersonaje(personaje), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Personaje> actualizarPersonaje(
+            @PathVariable Integer id,
+            @RequestBody Personaje personaje
+    ){
+        Personaje newPersonaje = personajeService.obtenerPersonajexId(id)
+                .orElseThrow(() -> new ResourceNotFoundException("El personaje con Id "
+                        + id +" no existe"));
+        newPersonaje.setNomPersonaje(personaje.getNomPersonaje());
+        newPersonaje.setApePersonaje(personaje.getApePersonaje());
+        newPersonaje.setFechNacPersonaje(personaje.getFechNacPersonaje());
+        return new ResponseEntity<>(
+                personajeService.guardarPersonaje(newPersonaje),
+                HttpStatus.OK);
+    }
 }
--- src/main/java/pe/edu/cibertec/T2_SW_AlexandraVilchez/controller/ProgramaTvController.java
+++ src/main/java/pe/edu/cibertec/T2_SW_AlexandraVilchez/controller/ProgramaTvController.java
@@ -1,4 +1,62 @@
 package pe.edu.cibertec.T2_SW_AlexandraVilchez.controller;
 
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pe.edu.cibertec.T2_SW_AlexandraVilchez.exception.ResourceNotFoundException;
+import pe.edu.cibertec.T2_SW_AlexandraVilchez.model.bd.ProgramaTv;
+import pe.edu.cibertec.T2_SW_AlexandraVilchez.service.ProgramaTvService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping(path = "api/v1/programatv")
 public class ProgramaTvController {
+    private ProgramaTvService programaTvService;
+
+    @GetMapping("")
+    public ResponseEntity<List<ProgramaTv>> listarProgramaTv(){
+        List<ProgramaTv> programaTvList = new ArrayList<>(programaTvService.listarProgramaTv());
+        if(programaTvList.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(programaTvList, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProgramaTv> obtenerProgramaTvXId(
+            @PathVariable Integer id){
+        ProgramaTv programaTv = programaTvService
+                .obtenerProgramaTvxId(id).orElseThrow(
+                        () -> new ResourceNotFoundException("El programa con Id " +
+                                id + " no existe"));
+        return new ResponseEntity<>(programaTv, HttpStatus.OK);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<ProgramaTv> registrarProgramaTv(
+            @RequestBody ProgramaTv programaTv
+    ){
+        return new ResponseEntity<>(
+                programaTvService.guardarProgramaTv(programaTv), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProgramaTv> actualizarProgramaTv(
+            @PathVariable Integer id,
+            @RequestBody ProgramaTv programaTv
+    ){
+        ProgramaTv newProgramaTv = programaTvService.obtenerProgramaTvxId(id)
+                .orElseThrow(() -> new ResourceNotFoundException("El programa con Id "
+                        + id +" no existe"));
+        newProgramaTv.setTitulo(programaTv.getTitulo());
+        newProgramaTv.setResumen(programaTv.getResumen());
+        newProgramaTv.setFechaInicio(programaTv.getFechaInicio());
+        return new ResponseEntity<>(
+                programaTvService.guardarProgramaTv(newProgramaTv),
+                HttpStatus.OK);
+    }
 }
